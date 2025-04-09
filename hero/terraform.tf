5e84596752f0148d8a1c142d1734a3c09324b182
VishalJTalaviya <144648163+VishalJTalaviya@users.noreply.github.com>
2025-04-09 10:43:36 +0530

    update terraform

--- hero/terraform.tf
+++ hero/terraform.tf
@@ -5,4 +5,12 @@ terraform {
         version = "5.91.0"
     }
   }
+
+  backend "s3" {
+    
+    bucket = "vishal-state-bucket"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "vishal-state-table"
+  }
 }
